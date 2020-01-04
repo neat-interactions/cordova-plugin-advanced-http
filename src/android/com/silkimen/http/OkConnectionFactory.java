b12f666d6c6a857bb900714da2b31347e20a1138
Bastian Brandt <bastian@brandt.it>
2020-01-04 15:28:26 +0100

    Force HTTP/1.1 for all requests

--- src/android/com/silkimen/http/OkConnectionFactory.java
+++ src/android/com/silkimen/http/OkConnectionFactory.java
@@ -2,14 +2,16 @@ package com.silkimen.http;
 
 import okhttp3.OkHttpClient;
 import okhttp3.OkUrlFactory;
+import okhttp3.Protocol;
 
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.URLStreamHandler;
 import java.net.Proxy;
+import java.util.Arrays;
 
 public class OkConnectionFactory implements HttpRequest.ConnectionFactory {
-  private final OkHttpClient client = new OkHttpClient();
+  private final OkHttpClient client = new OkHttpClient().newBuilder().protocols(Arrays.asList(Protocol.HTTP_1_1)).build();
 
   public HttpURLConnection create(URL url) {
     OkUrlFactory urlFactory = new OkUrlFactory(this.client);
